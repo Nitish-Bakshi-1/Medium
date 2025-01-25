6d57fef2c774f204cdfd9b811a47aea9403621b9
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-26 00:29:56 +0530

    Bootstrapped fe

--- medium-client/src/App.tsx
+++ medium-client/src/App.tsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Blog from "./pages/Blog";
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
